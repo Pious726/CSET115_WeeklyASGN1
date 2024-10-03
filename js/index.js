69d7d90894b57874392b890762cb8bd6023e4098
Tyler M. <tjmcdowell726@gmail.com>
2024-10-03 08:52:41 -0400

    Created functions to solve equations

--- js/index.js
+++ js/index.js
@@ -12,20 +12,27 @@ function enterEvent(buttonPress) {
         || buttonPress.key == "4" || buttonPress.key == "5"
         || buttonPress.key == "6" || buttonPress.key == "7"
         || buttonPress.key == "8" || buttonPress.key == "9"
-        || buttonPress.key == "+"
-    ) {
-        document.getElementById("input").value += buttonPress
+        || buttonPress.key == "+" || buttonPress.key == "-"
+        || buttonPress.key == "*" || buttonPress.key == "/"
+        || buttonPress.key == "^2" || buttonPress.key == "√") 
+        {
+        document.getElementById("input").value += buttonPress.key
     }
 }
 
 
 // Functions for performing action
-function equalsButton(firstNumber, secondNumber) {
-    if (operation == "+") {
-        result.innerHTML = firstNumber + secondNumber;
+function equalsButton() {
+    if (buttonPress.key == "enter") {
+        let x = document.getElementById("result").value;
+        solveEquation();
     }
 }
-
+function solveEquation() {
+    let x = document.getElementById("input").value;
+    let y = math.evaluate(x)
+    document.getElementById("result").value = y
+}
 
 // Functions to clear the calculator
 function clearInput() {
